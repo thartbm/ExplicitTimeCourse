bef6ca18fdd6609809d032bcdd10776d42333dee
Marius 't Hart <thartbm@gmail.com>
2024-11-05 09:43:59 -0500

    download data sets from older projects

---
+++ R/data.R
@@ -0,0 +1,33 @@
+
+# previous data sets -----
+
+# download old data
+
+downloadAddivityAiming <- function() {
+  
+  dir.create(file.path('data', 'additivity'))
+  Reach::downloadOSFdata( repository = 'kr5eh',
+                          filelist = c('/'='aiming.zip'),
+                          folder = 'data/additivity/',
+                          overwrite = TRUE,
+                          unzip = TRUE,
+                          removezips = TRUE)
+  
+}
+
+downloadImplicitAiming <- function() {
+  
+  dir.create(file.path('data', 'implicit'))
+  Reach::downloadOSFdata( repository = 'ajwyr',
+                          filelist = list('data'=c('exp4.zip','demographics.csv')), # maybe processed.zip is easier?
+                          folder = 'data/implicit/',
+                          overwrite = TRUE,
+                          unzip = TRUE,
+                          removezips = TRUE)
+  
+  demographics <- read.csv('data/implicit/demographics.csv', stringsAsFactors = TRUE)
+  demographics <- demographics[which(demographics$exp == 4),]
+  write.csv(demographics, 'data/implicit/demographics.csv', quote=F, row.names=F)
+  
+}
+
